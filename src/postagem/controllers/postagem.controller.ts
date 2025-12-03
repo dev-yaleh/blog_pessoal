334836032fa75ccf261a2222322d151ca4e88fe7
Yaleh Nóbrega <dev.yaleh@gmail.com>
2025-12-03 15:11:02 -0300

    Postagem, Controller e Service

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,6 +1,7 @@
+import { DeleteResult } from 'typeorm';
 import { Postagem } from '../entities/model.entities';
 import { PostagemService } from './../services/postagem.service';
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 
 @Controller("/postagens") // Indica que Classe é uma Controller
 export class PostagemController {
@@ -13,5 +14,45 @@ findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll(); // Chama o método findAll do serviço de postagem
 }
 
+// @Get("/:id_post") Indica que esse método lida com Requisições do Tipo GET e que no seu endpoint será enviado um id como parametro
+// @Param captura o paramêtro envia pelo endpoint e o atribui ao parametro do método findById(id:number)
+// ParseIntPipe converte o parametro do endpoint de string para int. Ex: id: '1' => id: 1
+
+@Get("/:id")
+@HttpCode(HttpStatus.OK) // Define o código de status HTTP para 200 OK  
+findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.postagemService.findById(id); // Chama o método findAll do serviço de postagem
 }
 
+@Get('/titulo/:titulo') // postagens/titulo/{texto}
+    @HttpCode(HttpStatus.OK)
+    findByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+        return this.postagemService.findByTitulo(titulo);
+    }
+
+    // @Post() Indica que esse método lida com Requisições do Tipo Post
+    // @Body() Captura/Extrai o objeto que vem pelo Corpo da Requisição e passa para parametro do método 
+    @Post() // Usado quando queremos Cadastrar/Criar/Salva alguma informação
+    @HttpCode(HttpStatus.CREATED)   // Monta a Resposa HTTP para o Cliente com o status 201
+    create(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.create(postagem);
+    }
+
+    // @Put() Indica que esse método lida com Requisições do Tipo Put
+    // @Body() Captura/Extrai o objeto que vem pelo Corpo da Requisição e passa para parametro do método 
+    @Put()// Usado quando queremos Atualizar alguma informação
+    @HttpCode(HttpStatus.OK) // Monta a Resposa HTTP para o Cliente com o status 200    
+    update(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.update(postagem);
+    }
+
+    // @Delete('/:ID') Indica que esse método lida com Requisições do Tipo DELETE e que no seu endpoint será enviado um id como parametro
+    // @Param captura o paramêtro envia pelo endpoint e o atribui ao parametro do método delete(id:number)
+    // ParseIntPipe converte o parametro do endpoint de string para int. Ex: id: '1' => id: 1
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT) // Monta a Resposa HTTP para o Cliente com o status 204
+    delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult>{
+        return this.postagemService.delete(id);
+    }
+
+}
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Postagem } from "../entities/model.entities";
-import { Repository } from "typeorm";
+import { ILike, Repository } from "typeorm";
+import { DeleteResult } from "typeorm/browser";
+
+/*
+    @Injectable: Indica que é uma Classe de Serviço e pode ser inserida/injetada 
+    diretamente em outras classes sem a necessidade de instância
+
+    @InjectRepository(Postagem): Decorator que inverte a dependência da Classe(Repository).
+    Com isso, podemos criar objetos de Classes Repository sem precisar instanciar objetos.
+    Além disso, indica ao Nest que a nossa Repository aponta para a Entidade Postagem, isto é,
+    os métodos de manipulação de BD dentro da Repository serão direcionados a tabela tb_postagem
+*/
 
 @Injectable() // Indica que essa classe é de servço e pode ser inserida/injetada em outras classes
 export class PostagemService {
@@ -12,13 +23,85 @@ export class PostagemService {
         private postagemRepository: Repository<Postagem>
         
 
-    ) {}
+    ) { }
  
 async findAll(): Promise<Postagem[]> {
-
     return this.postagemRepository.find()
 
 }
+
+// Se a postagem não existir, lace uma Exceção que vai direto para o Cliente com o status 404 Not Found
+async findById(id: number): Promise<Postagem> {
+    const postagem = await this.postagemRepository.findOne({
+        where: { id }
+    })
+
+    if (!postagem) {
+        throw new HttpException('Postagem not found', HttpStatus.NOT_FOUND);
+    }
+    // Se a postagem foi encontrada, retorna ela
+    return postagem;
+}
+
+ async findByTitulo(titulo: string): Promise<Postagem[]> {
+        // Verifica se existi postagem com o parametro informado
+        return await this.postagemRepository.find({
+            where: {
+                titulo: ILike(`%${titulo}%`)
+            }
+        })
+    }
+
+    async create(postagem: Postagem): Promise<Postagem> {
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async update(postagem: Postagem): Promise<Postagem> {
+
+        /*
+            {
+                "id": 1
+                "titulo": "",
+                "texto": "Texto da Postagem 3"
+            }
+        */
+
+        // Chama o método findById anteriro para pesquisar uma postagem pelo id extraido do objeto postagem
+        let buscaPostagem = await this.findById(postagem.id);
+
+        // Se a postagem não existir, lace uma Exceção que vai direto para o Cliente com o status 404 Not Found
+        if (!buscaPostagem || !postagem.id) {
+            throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+        }
+
+        // Se a postagem foi encontrada, cadastra ela no BD e retorna ela
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+
+        // Chama o método findById anteriro para pesquisar uma postagem pelo id extraido do objeto postagem
+        let buscaPostagem = await this.findById(id);
+
+        // Se a postagem não existir, lace uma Exceção que vai direto para o Cliente com o status 404 Not Found
+        if (!buscaPostagem)
+            throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+
+        // Se a postagem foi encontrada, apaga ela no BD e retorna uma confirmação de exclusão
+        return await this.postagemRepository.delete(id);
+
+    }
+
+}
+
+/* REPOSITORY<POSTAGEM>
+
+    find() => SELECT * FROM tb_postagens
+    save() => INSERT INTO tb_postagens VALUES (titulo, texto, data)
+
+    findOne() => SELECT * FROM tb_postagens WHERE id = {id}
+
+*/
 
 /*
     pivate idade: number
@@ -28,5 +111,3 @@ async findAll(): Promise<Postagem[]> {
     
 
 */
-
-}
